d098cd600f72867c64dbca28d623945d72358501
imsujeet <iamsujeet1308@gmail.com>
2021-10-13 10:21:24 +0530

    add /hello

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+})
+
+app.get('/hello', (req, res) => {
+  res.send('Hello Again!');
 })
 
 app.listen(port, () => {
